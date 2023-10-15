10d2b6697ff84cdf83eca4a5cb461c0f0751fc16
Joao <joaopaulo.cunha@outlook.com>
2023-10-15 00:14:08 -0300

    transaction

---
+++ lib/models/transaction.dart
@@ -0,0 +1,15 @@
+class Trasaction {
+  final String id;
+  final String tittle;
+  final double value;
+  final DateTime date;
+
+  Trasaction({
+   required this.id, 
+   required this.tittle, 
+   required this.value,
+   required this.date
+  });
+
+  
+}
